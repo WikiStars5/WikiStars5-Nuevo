27ed4e2e03ad5db50e52d0291d6819d7a3dec8bc
Wikibot1 <wikibot1.000@gmail.com>
2025-10-19 20:03:19 +0000

    creo que me di cuenta del error, parece que lo que estamso desarrollando

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6468404683-437dc",
-  "appId": "1:1031255223827:web:0c0c4cd95a8a351281d9e6",
-  "apiKey": "AIzaSyDldME8i_o1lp7MBKUcoK5pYi_Oy_kyVZk",
-  "authDomain": "studio-6468404683-437dc.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1031255223827"
+  "projectId": "wikistars5-nuevo",
+  "appId": "1:480215928523:web:a5de1569959fadc56fd54e",
+  "apiKey": "AIzaSyBy0a6uH3AyezH47bXHJd33fFopBmAiV1E",
+  "authDomain": "wikistars5-nuevo.firebaseapp.com",
+  "storageBucket": "wikistars5-nuevo.appspot.com",
+  "messagingSenderId": "480215928523",
+  "measurementId": "G-NWYPQGSDMP"
 };
