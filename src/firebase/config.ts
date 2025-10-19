7c1e054e47d158269bc9601844d00957e7036ab8
Wikibot1 <wikibot1.000@gmail.com>
2025-10-19 19:26:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6468404683-437dc",
+  "appId": "1:1031255223827:web:0c0c4cd95a8a351281d9e6",
+  "apiKey": "AIzaSyDldME8i_o1lp7MBKUcoK5pYi_Oy_kyVZk",
+  "authDomain": "studio-6468404683-437dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1031255223827"
+};
