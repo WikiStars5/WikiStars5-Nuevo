e4bab677f217270b187e42ae48a6cfdb43962e1b
Wikibot1 <wikibot1.000@gmail.com>
2025-10-19 22:32:16 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/(main)/figures/[id]/client-page.tsx
+++ src/app/(main)/figures/[id]/client-page.tsx
@@ -63,7 +63,7 @@ export default function FigureDetailClient({ figureId }: { figureId: string }) {
               Actitud
             </TabsTrigger>
             <TabsTrigger value="emocion">
-             <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-4 w-4" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" /><path d="M9 10h.01" /><path dM15 10h.01" /><path d="M9.5 15a3.5 3.5 0 0 0 5 0" /></svg>
+             <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-4 w-4" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 21a9 9 0 1 1 0 -18a9 9 0 0 1 0 18z" /><path d="M9 10h.01" /><path d="M15 10h.01" /><path d="M9.5 15a3.5 3.5 0 0 0 5 0" /></svg>
               Emoción
             </TabsTrigger>
             <TabsTrigger value="rachas">
