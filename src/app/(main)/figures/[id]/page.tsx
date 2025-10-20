f98831fc96838b144a9efa9eece035f2622deb0c
Wikibot1 <wikibot1.000@gmail.com>
2025-10-20 22:31:49 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/(main)/figures/[id]/page.tsx
+++ src/app/(main)/figures/[id]/page.tsx
@@ -1,6 +1,6 @@
 import FigureDetailClient from './client-page';
 
-export default function FigureDetailPage({ params }: { params: { id: string } }) {
+export default async function FigureDetailPage({ params }: { params: { id: string } }) {
   // Data fetching is now handled on the client side.
   // We just pass the figure ID to the client component.
   return <FigureDetailClient figureId={params.id} />;
