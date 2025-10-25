f7c667eaa7f3150018b18648c14b9359d7087484
Wikibot1 <wikibot1.000@gmail.com>
2025-10-25 01:08:18 +0000

    asi me gusta el tamaño, pero no se porque se ve algo borrosa, si antes t

--- src/components/figure/goat-battle.tsx
+++ src/components/figure/goat-battle.tsx
@@ -289,7 +289,7 @@ export default function GoatBattle() {
         </CardTitle>
         <CardDescription className="max-w-md flex flex-col items-center text-center gap-2">
             <span>¿Quién es el mejor de todos los tiempos? El ganador obtiene este ícono en su perfil.</span>
-            <Image src={GOAT_ICON_URL} alt="GOAT Icon" width={40} height={40} className="h-10 w-10" />
+            <Image src={GOAT_ICON_URL} alt="GOAT Icon" width={40} height={40} className="h-10 w-10" unoptimized />
         </CardDescription>
         {isBattleOver ? (
             <div className="font-bold text-lg text-primary">¡La votación ha terminado!</div>
