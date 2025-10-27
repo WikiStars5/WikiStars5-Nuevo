cc82c6c52f5d2f9eb95f60536ebacd2b43e1a5c0
Wikibot1 <wikibot1.000@gmail.com>
2025-10-27 02:15:51 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
-import { Providers } from './providers';
+import { ThemeProvider } from 'next-themes';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { StreakAnimationProvider } from '@/context/StreakAnimationContext';
 import StreakAnimationOverlay from '@/components/streaks/StreakAnimationOverlay';
 
 const logoUrl = "https://firebasestorage.googleapis.com/v0/b/wikistars5-nuevo.firebasestorage.app/o/logo%2Flogodia.png?alt=media&token=fb7367da-8db6-4f1d-a1f0-d03f57e6b9f6";
@@ -46,11 +48,20 @@ export default function RootLayout({
         <meta name="theme-color" content="#f4f4f5" media="(prefers-color-scheme: light)" />
       </head>
       <body className={cn('min-h-screen bg-background font-body antialiased')}>
-        <Providers>
-          {children}
-          <Toaster />
-          <StreakAnimationOverlay />
-        </Providers>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          forcedTheme="dark"
+          disableTransitionOnChange
+        >
+          <FirebaseClientProvider>
+            <StreakAnimationProvider>
+              {children}
+              <Toaster />
+              <StreakAnimationOverlay />
+            </StreakAnimationProvider>
+          </FirebaseClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
