4d33f8dba8a8f5ce99564678364f25f17d288e67
Wikibot1 <wikibot1.000@gmail.com>
2025-11-03 21:13:52 +0000

    he probado la opcion y no veo cambio, al arecer falta haerlo funcional (

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          forcedTheme="dark"
           disableTransitionOnChange
         >
           <FirebaseClientProvider>
