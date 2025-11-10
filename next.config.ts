639110c50b2bf1de1f99112b18b6b7604dcf93c5
Wikibot1 <wikibot1.000@gmail.com>
2025-11-10 15:59:55 +0000

    oh no, otra vez no: starting build "a373d5fc-7460-46e6-ae1d-b07187e088f

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 
 /** @type {import('next').NextConfig} */
 
+/*
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
+*/
 
 
 const nextConfig = {
@@ -76,4 +78,5 @@ const nextConfig = {
   }
 }
 
-module.exports = withPWA(nextConfig);
+// module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
