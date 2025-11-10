204551671f6d2e6a8a0284be359e7f1b4c0cf8ed
Wikibot1 <wikibot1.000@gmail.com>
2025-11-10 15:50:04 +0000

    Try fixing this error: `Runtime Error: Error: Separator is not defined.

--- src/components/figure/achievements.tsx
+++ src/components/figure/achievements.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState, useEffect } from 'react';
@@ -19,6 +20,7 @@ import {
   DialogDescription,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { Separator } from '@/components/ui/separator';
 
 interface AchievementsProps {
     figure: Figure;
