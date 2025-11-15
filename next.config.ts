aff9cf618cf0eba171d5f1a1309b2cbd5cb85a77
Wikibot1 <wikibot1.000@gmail.com>
2025-11-15 00:15:01 +0000

    ayuda chat, el boton para descargar el pwa no parece funcionar, en telef

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 
 /** @type {import('next').NextConfig} */
 
-/*
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
-*/
 
 
 const nextConfig = {
@@ -78,5 +76,4 @@ const nextConfig = {
   }
 }
 
-// module.exports = withPWA(nextConfig);
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
