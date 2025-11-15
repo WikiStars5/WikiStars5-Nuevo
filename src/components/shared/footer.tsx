49720738e985c0458743f3bae77283c59423e014
Wikibot1 <wikibot1.000@gmail.com>
2025-11-15 17:51:53 +0000

    elimina esto (_for element p_)

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -11,10 +11,8 @@ export default function Footer() {
           <Image src="https://firebasestorage.googleapis.com/v0/b/wikistars5-nuevo.firebasestorage.app/o/logo%2Flogodia.png?alt=media&token=fb7367da-8db6-4f1d-a1f0-d03f57e6b9f6" alt="WikiStars5 Logo" width={24} height={24} className="h-6 w-6" />
           <span className="font-headline text-primary">WikiStars5</span>
         </div>
-        <p className="text-sm text-muted-foreground text-center order-last md:order-none">
-          &copy; 2025 WikiStars5. Todos los derechos reservados.
-        </p>
-        <nav className="flex items-center justify-center md:justify-end gap-x-4 text-sm font-medium">
+        
+        <nav className="flex items-center justify-center md:justify-end gap-x-4 text-sm font-medium col-start-1 md:col-start-3">
           <Link href="/admin/login" className="text-muted-foreground transition-colors hover:text-foreground">
             Admin
           </Link>
