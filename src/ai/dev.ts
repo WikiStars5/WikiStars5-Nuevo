674b5a98583a2d7555a1cf92d348b91cbd82d057
Wikibot1 <wikibot1.000@gmail.com>
2025-12-02 17:30:42 +0000

    la 2 y la 3 es ago que nunca pedi

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,8 +1,6 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/suggest-related-figures.ts';
-import '@/ai/flows/analyze-comment-sentiment.ts';
 import '@/ai/flows/verify-wikipedia-character.ts';
 import '@/ai/flows/verify-famous-birthdays-character.ts';
 import '@/ai/flows/verify-domain.ts';
