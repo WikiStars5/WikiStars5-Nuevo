7519ecb545c1e19fc98065326e6405d28e919833
Wikibot1 <wikibot1.000@gmail.com>
2026-01-25 21:12:33 +0000

    1. Detalles a mejorar Falta el evento de clic: Si no añades el notificat

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,4 @@
-// Give the service worker a name
-const swName = 'firebase-messaging-sw';
 
-// To make this file work, you need to place it in the public folder
-
-// Import and configure the Firebase SDK
-// It's safe to expose these keys. For more info, see:
-// https://firebase.google.com/docs/projects/learn-more#config-files-objects
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
@@ -20,19 +13,24 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-// Get an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+  console.log('Mensaje en segundo plano:', payload);
 
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.notification?.title || "WikiStars5";
   const notificationOptions = {
-    body: payload.notification.body,
+    body: payload.notification?.body || "Nueva actualización disponible",
     icon: 'https://firebasestorage.googleapis.com/v0/b/wikistars5-nuevo.firebasestorage.app/o/logo%2Flogodia%20(2).png?alt=media&token=7cdac6ec-4db8-4bda-a104-fa636e201528'
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// Open your web app when a notification is clicked
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
 });
