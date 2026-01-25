626a55545604f1f7897b69ad3bfc04bdee03f67e
Wikibot1 <wikibot1.000@gmail.com>
2026-01-25 21:10:48 +0000

    1. El nuevo sw.js debe ser tu firebase-messaging-sw.js La IA va a limpia

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,37 @@
-// This service worker is for Firebase Cloud Messaging.
+// Give the service worker a name
+const swName = 'firebase-messaging-sw';
 
-// Using compat scripts for broader browser support in service workers.
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.9.0/firebase-messaging-compat.js');
+// To make this file work, you need to place it in the public folder
 
-// Initialize the Firebase app in the service worker.
-// This config is safe to be public.
-firebase.initializeApp({
-    apiKey: "AIzaSyBy0a6uH3AyezH47bXHJd33fFopBmAiV1E",
-    authDomain: "wikistars5-nuevo.firebaseapp.com",
-    projectId: "wikistars5-nuevo",
-    storageBucket: "wikistars5-nuevo.appspot.com",
-    messagingSenderId: "480215928523",
-    appId: "1:480215928523:web:a5de1569959fadc56fd54e",
-    measurementId: "G-NWYPQGSDMP"
-});
+// Import and configure the Firebase SDK
+// It's safe to expose these keys. For more info, see:
+// https://firebase.google.com/docs/projects/learn-more#config-files-objects
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-// Retrieve an instance of Firebase Messaging to handle background messages.
+const firebaseConfig = {
+  apiKey: "AIzaSyBy0a6uH3AyezH47bXHJd33fFopBmAiV1E",
+  authDomain: "wikistars5-nuevo.firebaseapp.com",
+  projectId: "wikistars5-nuevo",
+  storageBucket: "wikistars5-nuevo.firebasestorage.app",
+  messagingSenderId: "480215928523",
+  appId: "1:480215928523:web:a5de1569959fadc56fd54e",
+  measurementId: "G-NWYPQGSDMP"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Get an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('[sw.js] Received background message: ', payload);
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
 
-  const notificationTitle = payload.notification?.title || 'Nueva Notificación';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification?.body || '',
-    icon: payload.notification?.image || '/icon.png', // A fallback icon
+    body: payload.notification.body,
+    icon: 'https://firebasestorage.googleapis.com/v0/b/wikistars5-nuevo.firebasestorage.app/o/logo%2Flogodia%20(2).png?alt=media&token=7cdac6ec-4db8-4bda-a104-fa636e201528'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
