c3d3f31c91a72c6c8cd8a056b7ccdac10a942ad4
Wikibot1 <wikibot1.000@gmail.com>
2026-03-07 02:51:25 +0000

    desactivor de anuncios

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,8 @@
 // Configuración del Service Worker para Notificaciones Push de Monetag
-self.options = {
+{/*self.options = {
     "domain": "5gvci.com",
     "zoneId": 10688478
 };
 self.lary = "";
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
+*/}
