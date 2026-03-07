e3cac8957a1a2732e6222a9bade646b9b721e6f5
Wikibot1 <wikibot1.000@gmail.com>
2026-03-07 12:28:18 +0000

    hay algo que no entiendo, ya desativamos las publiciades pero igual sigu

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,14 @@
-// Configuración del Service Worker para Notificaciones Push de Monetag
-{/*self.options = {
+/**
+ * CONFIGURACIÓN DE MONETAG (PAUSADA)
+ * He comentado este código para que el Service Worker deje de enviar notificaciones push,
+ * pero conservando la configuración para cuando decidas reactivarlo.
+ */
+
+/*
+self.options = {
     "domain": "5gvci.com",
     "zoneId": 10688478
-};
-self.lary = "";
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
-*/}
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+*/
